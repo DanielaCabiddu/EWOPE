7f38f32932e86829b7d33d0c3f7ff84db040bea1
DanielaCabiddu <daniela.cabiddu@ge.imati.cnr.it>
2025-07-31 15:44:02 +0200

    copyright

--- include/flag/flag.h
+++ include/flag/flag.h
@@ -1,3 +1,33 @@
+/********************************************************************************
+*  This file is part of EWoPE                                                   *
+*  Copyright(C) 2025: Daniela Cabiddu, Marianna Miola                           *
+*                                                                               *
+*  Author(s):                                                                   *
+*                                                                               *
+*     Daniela Cabiddu (daniela.cabiddu@cnr.it)                                  *
+*     Marianna Miola (marianna.miola@cnr.it)                                    *
+*                                                                               *
+*     Italian National Research Council (CNR)                                   *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)    *
+*     Via de Marini, 6                                                          *
+*     16149 Genoa,                                                              *
+*     Italy                                                                     *
+*                                                                               *
+*  This program is free software: you can redistribute it and/or modify it      *
+*  under the terms of the GNU General Public License as published by the        *
+*  Free Software Foundation, either version 3 of the License, or (at your       *
+*  option) any later version.                                                   *
+*                                                                               *
+*  This program is distributed in the hope that it will be useful, but          *
+*  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY   *
+*  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for  *
+*  more details.                                                                *
+*                                                                               *
+*  You should have received a copy of the GNU General Public License along      *
+*  with this program. If not, see <https://www.gnu.org/licenses/>.              *
+*                                                                               *
+*********************************************************************************/
+
 #ifndef FLAG_H
 #define FLAG_H
 
--- include/metadata/dependencies.cpp
+++ include/metadata/dependencies.cpp
@@ -1,14 +1,34 @@
-#include "dependencies.h"
-//#include "muselib/input/load_xyz.h"
+/********************************************************************************
+*  This file is part of EWoPE                                                   *
+*  Copyright(C) 2025: Daniela Cabiddu, Marianna Miola                           *
+*                                                                               *
+*  Author(s):                                                                   *
+*                                                                               *
+*     Daniela Cabiddu (daniela.cabiddu@cnr.it)                                  *
+*     Marianna Miola (marianna.miola@cnr.it)                                    *
+*                                                                               *
+*     Italian National Research Council (CNR)                                   *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)    *
+*     Via de Marini, 6                                                          *
+*     16149 Genoa,                                                              *
+*     Italy                                                                     *
+*                                                                               *
+*  This program is free software: you can redistribute it and/or modify it      *
+*  under the terms of the GNU General Public License as published by the        *
+*  Free Software Foundation, either version 3 of the License, or (at your       *
+*  option) any later version.                                                   *
+*                                                                               *
+*  This program is distributed in the hope that it will be useful, but          *
+*  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY   *
+*  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for  *
+*  more details.                                                                *
+*                                                                               *
+*  You should have received a copy of the GNU General Public License along      *
+*  with this program. If not, see <https://www.gnu.org/licenses/>.              *
+*                                                                               *
+*********************************************************************************/
 
-// #include "muselib/metadata/data_meta.h"
-// #include "muselib/metadata/metadata.h"
-// #include "muselib/metadata/extraction_meta.h"
-// #include "muselib/metadata/manipulate_meta.h"
-// #include "muselib/metadata/surface_meta.h"
-// #include "muselib/metadata/volume_meta.h"
-// #include "muselib/metadata/vario_meta.h"
-// #include "muselib/metadata/compute_meta.h"
+#include "dependencies.h"
 
 #include <fstream>
 
