09644722b8e9c7cf221aebc102d44a420ae85ce6
Daniela Cabiddu <daniela.cabiddu@cnr.it>
2025-07-31 15:37:01 +0200

    Update graph.h
    copyright and thanks

--- include/data_structures/graph.h
+++ include/data_structures/graph.h
@@ -1,3 +1,38 @@
+/********************************************************************************
+*  This file is part of EWoPE                                                   *
+*  Copyright(C) 2025: Daniela Cabiddu, Marianna Miola                           *
+*                                                                               *
+*  Author(s):                                                                   *
+*                                                                               *
+*     Daniela Cabiddu (daniela.cabiddu@cnr.it)                                  *
+*     Marianna Miola (marianna.miola@cnr.it)                                    *
+*                                                                               *
+*     Italian National Research Council (CNR)                                   *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)    *
+*     Via de Marini, 6                                                          *
+*     16149 Genoa,                                                              *
+*     Italy                                                                     *
+*                                                                               *
+*  This program is free software: you can redistribute it and/or modify it      *
+*  under the terms of the GNU General Public License as published by the        *
+*  Free Software Foundation, either version 3 of the License, or (at your       *
+*  option) any later version.                                                   *
+*                                                                               *
+*  This program is distributed in the hope that it will be useful, but          *
+*  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY   *
+*  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for  *
+*  more details.                                                                *
+*                                                                               *
+*  You should have received a copy of the GNU General Public License along      *
+*  with this program. If not, see <https://www.gnu.org/licenses/>.              *
+*                                                                               *
+*********************************************************************************/
+
+/***
+ We thank https://www.techiedelight.com/graph-implementation-using-stl/ for providing the original code used in this file.
+ We slightly modified it to suit the needs and purposes of the EWoPe project.
+***/
+
 #ifndef GRAPH_H
 #define GRAPH_H
 
